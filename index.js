5ee517b5bb71de61ae12b6337c59447f95d19db7
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-20 19:57:04 +0530

    v1.4

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!tu')
+  return res.send('Hello World!tu')
 })
 
 app.listen(3000, () => {
   console.log('server running at port 3000')
 })
+
+module.exports = app
