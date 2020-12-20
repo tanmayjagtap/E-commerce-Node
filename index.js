34d66fbb7be1e86b621295df090251740e86998b
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-20 19:19:27 +0530

    eslint added v1.2

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!tu')
+app.get('/', (req, res) => {
+  res.send('Hello World!tu')
 })
 
-app.listen(3000,()=>{
-    console.log('server running at port 3000');
+app.listen(3000, () => {
+  console.log('server running at port 3000')
 })
-
